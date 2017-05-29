f3d8c4d32899666321ad2e819fd08130867eac62
warlyware <danward@gmail.com>
2017-05-29 16:08:55 -0400

    Improve shifting color background

--- src/app/application.module.js
+++ src/app/application.module.js
@@ -3,44 +3,64 @@ angular.module('tripow', ['ngMaterial']);
 angular.module('tripow').run(() => {
     console.log('i am angular!');
 
+    var colors = [
+        {
+            name: 'blue',
+            hex: '#71AAD5'
+        }, {
+            name: 'grey',
+            hex: '#CFCFCF'
+        }
+    ];
+
     let canvas;
-    let colorA;
-    let colorB;
+    let currentColor;
+    let nextColor;
     let lerpIndex = 0;
-    let switching = false;
+    let shifted = false;
     let counter;
 
     let sketch = (sketchInstance) => {
         sketchInstance.setup = () => {
-            colorA  = sketchInstance.color('#71AAD5');
-            colorB  = sketchInstance.color('#CFCFCF');
+            currentColor = colors[0];
+            nextColor = colors[1];
             canvas = sketchInstance.createCanvas(window.innerWidth, window.innerHeight);
         }
 
         sketchInstance.draw = () => {
             let bgColor;
-            counter = (sketchInstance.millis() % 5000)/5000.0;
-            if (counter > 0.95) {
-                lerpIndex = 0;
-                switching = true;
-                swapColors(colorA, colorB);
-            } else {
-                lerpIndex = counter;
-                bgColor = sketchInstance.lerpColor(colorA, colorB, lerpIndex);
-                sketchInstance.background(bgColor);
-            }
+            counter = (sketchInstance.millis() / 10000) % 1;
+            lerpIndex = counter;
             console.log('counter', counter);
             console.log('lerpIndex', lerpIndex);
+            if (counter >= 0.95) {
+                lerpIndex = 0;
+                if (!shifted) {
+                    shiftColors(sketchInstance);
+                    sketchInstance.background(sketchInstance.color(currentColor.hex));
+                    shifted = true;
+                }
+            } else {
+                shifted = false;
+                bgColor = sketchInstance.lerpColor(
+                    sketchInstance.color(currentColor.hex),
+                    sketchInstance.color(nextColor.hex),
+                    lerpIndex
+                );
+                sketchInstance.background(bgColor);
+            }
         }
     }
-
-    function swapColors() {
-        let tempA = colorA;
-        let tempB = colorB;
-        colorA = tempB;
-        colorB = tempA;
-    }
-
 	let myP5 = new p5(sketch);
+
+    function shiftColors(sketchInstance) {
+        if (currentColor.name == colors[0].name) {
+            currentColor = colors[1];
+            nextColor = colors[0];
+        } else {
+            currentColor = colors[0];
+            nextColor = colors[1];
+        }
+    }
 
 });
