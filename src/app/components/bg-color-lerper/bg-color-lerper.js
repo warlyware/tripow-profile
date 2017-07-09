1c31a127ac6d5f7044ebb144910091041a09aef8
warlyware <danward@gmail.com>
2017-07-09 12:13:40 -0400

    Support larger screen, prepare for restoring tripow element after menu

--- src/app/components/bg-color-lerper/bg-color-lerper.js
+++ src/app/components/bg-color-lerper/bg-color-lerper.js
@@ -57,6 +57,8 @@ export class BackgroundColorLerper {
         this.canvas = this.sketchInstance.createCanvas(window.innerWidth, window.innerHeight);
         this.canvas.parent('canvas');
         this.tripowElement = this.sketchInstance.select('#tripow');
+        this.originalTripowElement = {};
+        angular.copy(this.tripowElement, this.originalTripowElement);
         this.tripowElement.mousePressed(() => {
             this.handleTripowClick();
         });
@@ -97,7 +99,6 @@ export class BackgroundColorLerper {
     }
 
     fadeToBlack() {
-        console.log('trying to fade to black');
         if (this.fadeCounter < 1) {
             this.fadeCounter += .005;
             this.bgColor = this.sketchInstance.lerpColor(
@@ -108,6 +109,9 @@ export class BackgroundColorLerper {
             this.sketchInstance.background(this.bgColor);
         }
         if (this.fadeCounter > 0.1) {
+            if (this.fadeCounter > 1) {
+                this.tripowElement.remove();
+            }
             this.fontSize += 0.25;
             this.opacity -= 0.02;
             this.tripowElement.style('font-size', `${this.fontSize}px`);
