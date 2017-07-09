10427396c3d1bff10513b71b01ef8fe2a8e856d7
warlyware <danward@gmail.com>
2017-07-09 15:26:43 -0400

    Add youtube embed for video on menu

--- src/app/application.controller.js
+++ src/app/application.controller.js
@@ -3,9 +3,9 @@ import { BackgroundColorLerper } from './components/bg-color-lerper';
 angular.module('tripow')
     .controller('ApplicationController', ApplicationController);
 
-    ApplicationController.$inject = ['$timeout', '$mdMedia'];
+    ApplicationController.$inject = ['$timeout', '$mdMedia', '$mdDialog'];
 
-    function ApplicationController($timeout, $mdMedia) {
+    function ApplicationController($timeout, $mdMedia, $mdDialog) {
         let vm = this;
 
         vm.menuIsOn = false;
@@ -27,6 +27,17 @@ angular.module('tripow')
                 bgColorLerper.init();
                 bgColorLerper.run();
             }, 600);
+        }
+
+        vm.showVideoDialog = ($event) => {
+            $mdDialog.show({
+                template: `<div style="position:relative;height:0;padding-bottom:56.25%">
+                <iframe src="https://www.youtube.com/embed/XIaBehpmIoA?ecver=2" width="640" height="360"
+                frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe>
+                </div>`,
+                targetEvent: $event,
+                clickOutsideToClose: true,
+            });
         }
 
         return vm;
